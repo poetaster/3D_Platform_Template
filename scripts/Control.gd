d765ae5a76c5709a7464fb58c6be6757ce63b043
Mark Washeim <blueprint@poetaster.de>
2023-01-17 09:15:01 +0100

    Playable demos for Jolla.

--- scripts/Control.gd
+++ scripts/Control.gd
@@ -1,5 +1,6 @@
 extends Control
 
+onready var label = $Label
 
 func _ready():
 	pass 
@@ -7,3 +8,9 @@ func _ready():
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/Level.tscn")
+
+
+
+func _on_Button_button_up():
+	get_tree().change_scene("res://scenes/Level.tscn")
+
--- scripts/kineball.gd
+++ scripts/kineball.gd
@@ -1,5 +1,6 @@
 extends KinematicBody
 
+onready var label = Utils.get_main_node().get_node("HUD/uifb")
 
 
 var vec_pos = Vector3(0,0,0) 
@@ -8,46 +9,96 @@ var rotspeed= 9
 var gravity= -5
 var jump_force = 120
 
+# swipe vectors 
+#        "swipe_up"         : Vector2.UP,
+#        "swipe_up_right"   : Vector2.UP + Vector2.RIGHT,
+#        "swipe_right"      : Vector2.RIGHT,
+#        "swipe_down_right" : Vector2.DOWN + Vector2.RIGHT,
+#        "swipe_down"       : Vector2.DOWN,
+#        "swipe_down_left"  : Vector2.DOWN + Vector2.LEFT,
+#        "swipe_left"       : Vector2.LEFT,
+#        "swipe_up_left"    : Vector2.UP + Vector2.LEFT
+
 
 
 func _ready():
+	#var label = Utils.get_main_node().get_node("HUD/uifb")
+	var world = get_parent()
+	world.connect("jump", self, "_on_jump")
+
 #	global_transform.origin = get_parent().get_node("spawn test position").global_transform.origin
 	pass	
 
+func _input(event):
+	
+	#var label = Utils.get_main_node().get_node("HUD/uifb")
+	if event is InputEventScreenDrag:
+		if event.relative[0] > 0:
+			print(event.relative)
+			vec_pos.x=1
+			$ballMesh.rotate_z(deg2rad(-rotspeed))
+		elif event.relative[0] < 0:
+			vec_pos.x=-1
+			$ballMesh.rotate_z(deg2rad(rotspeed))
+		else:
+			vec_pos.x= lerp(vec_pos.x,0,0.2)
+			
+		if event.relative[1] < 0:
+			vec_pos.z=-1
+			$ballMesh.rotate_x(deg2rad(-rotspeed))
+		elif event.relative[1] > 0:
+			vec_pos.z=1
+			$ballMesh.rotate_x(deg2rad(rotspeed))
+		else:
+			vec_pos.z= lerp(vec_pos.z,0,0.2) 
+
+	vec_pos.y += gravity
+	
+	if (event is InputEventScreenTouch and
+		not InputEventScreenDrag):
+			vec_pos.y = jump_force
+	if event is InputEventSingleScreenSwipe:
+		vec_pos.y = jump_force
+			
+			
+	vec_pos = move_and_slide(vec_pos,Vector3.UP)
+		
 func _physics_process(_delta):
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+
+	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
 		vec_pos.x=0
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("right"):
 		vec_pos.x=speed
 		$ballMesh.rotate_z(deg2rad(-rotspeed))
-	elif Input.is_action_pressed("ui_left"):
-		vec_pos.x=-speed		
+	elif Input.is_action_pressed("left"):
+		vec_pos.x=-speed
 		$ballMesh.rotate_z(deg2rad(rotspeed))
 	else:
 		vec_pos.x= lerp(vec_pos.x,0,0.2)
 		
-	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("up") and Input.is_action_pressed("down"):
 		vec_pos.z=0
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("up"):
 		vec_pos.z=-speed
 		$ballMesh.rotate_x(deg2rad(-rotspeed))
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("down"):
 		vec_pos.z=speed
 		$ballMesh.rotate_x(deg2rad(rotspeed))
 	else:
 		vec_pos.z= lerp(vec_pos.z,0,0.2) 
-
 	vec_pos.y += gravity
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
 		vec_pos.y = jump_force
+#	if Input.is_action_pressed("")
 			
 	vec_pos = move_and_slide(vec_pos,Vector3.UP)
 
 
 func _on_Abyss_body_entered(body):
 	if body.name == "KineBall":
-		get_tree().change_scene("res://scenes/Level.tscn")
+		get_parent().get_node("Sounds/hurt").play()
+		$Timer2.start()
 
 
 func _on_Enemy_body_entered(body):
@@ -68,8 +119,10 @@ func _on_Timer_timeout():
 func _on_Timer2_timeout():
 	get_tree().change_scene("res://scenes/Level.tscn")
 
-
-
+# signal from Level/world
+func _on_jump():
+	vec_pos.y = jump_force
+	vec_pos = move_and_slide(vec_pos,Vector3.UP)
 
 
 func _on_Coin_cn_collected():
